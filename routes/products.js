f8cc7b424d108b8f1f7efa7c57d0bd7c2751d562
Lance Liam <lance.depadua@mnltechnology.com>
2019-08-26 22:21:29 +0800

    Products route completed

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,42 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 
+// # Functionality: GET a product
+// # Route:         localhost:5000/api/products
+// # isPrivate?:    true
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    if (!products) {
+      return res.status(500).json({ message: 'No Products found!' });
+    }
+
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error! ' });
+  }
+});
+
+// # Functionality: GET a product by ID
+// # Route:         localhost:5000/api/products/:id
+// # isPrivate?:    true
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(500).json({ message: 'No Product found!' });
+    }
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error! ' });
+  }
+});
+
 // # Functionality: CREATE a product
 // # Route:         localhost:5000/api/products
 // # isPrivate?:    true
@@ -50,6 +86,28 @@ router.put('/:id', async (req, res) => {
     });
 
     res.json({ message: 'Product updated!' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+// # Functionality: DELETE a product by ID if the user is admin only
+// # Route:         localhost:5000/api/products/:id
+// # isPrivate?:    true
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(500).json({ message: 'No Product found!' });
+    }
+
+    await product.delete();
+
+    res.json({ message: 'Product Deleted!' });
   } catch (error) {
     res.status(500).json({ message: 'Server Error' });
   }
